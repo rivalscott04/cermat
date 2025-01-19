f47d7045a7186c77e11076c9a8443fc4ab275fed
rivalscott04 <rival.biasrori@outlook.com>
2025-01-20 00:30:19 +0800

    edit landing page judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Landing Page</title>
+  <title>Mahir Cermat</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
   <style>
     .hero-section {
@@ -26,6 +26,7 @@
       color: white;
       font-size: 1.5rem;
       margin: 2rem 0;
+      font-weight: bold;
     }
 
     .laptop-container {
